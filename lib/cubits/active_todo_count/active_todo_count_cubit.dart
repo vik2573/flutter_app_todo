c2d197f0d4350dae298622bf9bbb78d48480036d
vik2573 <va-demen@yandeex.ru>
2022-04-17 12:52:21 +0400

    add active_todo_counter and todo_list

---
+++ lib/cubits/active_todo_count/active_todo_count_cubit.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import 'package:flutter_app_todo/cubits/todo_list/todo_list_cubit.dart';
+import 'package:flutter_app_todo/models/todo_model.dart';
+
+part 'active_todo_count_state.dart';
+
+class ActiveTodoCountCubit extends Cubit<ActiveTodoCountState> {
+  late final StreamSubscription todoListSubscription;
+  final TodoListCubit todoListCubit;
+  ActiveTodoCountCubit({
+    required this.todoListCubit,
+  }) : super(ActiveTodoCountState.initial()) {
+    todoListSubscription =
+        todoListCubit.stream.listen((TodoListState todoListState) {
+      print('todoListState: $todoListState');
+
+      final int currentActiveTodoCount = todoListState.todos
+          .where((Todo todo) => todo.completed)
+          .toList()
+          .length;
+
+      emit(state.copyWith(activeTodoCount: currentActiveTodoCount));
+    });
+  }
+  Future<void> close() {
+    todoListSubscription.cancel();
+    return super.close();
+  }
+}
